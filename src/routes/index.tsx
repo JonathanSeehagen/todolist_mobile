361c51d861ef328984fad9729a9f27dddc3ceab5
Jonathan <jonathanseehagen@gmail.com>
2020-06-01 20:26:41 -0300

    Adjust of loading ActivityIndicator

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,18 +8,15 @@ import AppRoutes from './app.routes';
 const Routes: React.FC = () => {
   const {signed, loading} = useAuth();
 
-  if (loading) {
-    //RNSplashScreen
-    //await new Promise((resolve) => setTimeout(resolve, 2000));
+  //RNSplashScreen
+  //await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    if (loading) {
-      return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <ActivityIndicator size="large" color="#999" />
-        </View>
-      );
-    }
+  if (loading) {
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ActivityIndicator size="large" color="#999" />
+    </View>;
   }
+  //}
 
   return signed ? <AppRoutes /> : <AuthRoutes />;
 };
